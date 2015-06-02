e0806e48b303003d79771ad576e54657d94f140f
Javier E. Martinez <javier-0517@hotmail.com>
2015-06-02 12:28:26 -0500

    Apply Send Form
    Files PHP and JS

---
+++ forms/send.php
@@ -0,0 +1,34 @@
+<?php
+	require '../PHPMailer/PHPMailerAutoload.php';
+	$mail = new PHPMailer;
+
+	$to='javier-0517@hotmail.com';//Correo de contacto
+
+	//Recolectar los datos del formulario
+	$nombre=$_POST['name'];
+	$email=$_POST['email'];
+	$asunto=$_POST['asunto'];
+	$mensaje=nl2br($_POST['mensaje']);
+
+	// echo $nombre.' - '.$email.'<br>'.$asunto.'<br>'.$mensaje
+	if($nombre=="" || $email=="" || $asunto=="" || $mensaje==""){
+		echo '<div class="alert alert-danger">Todos los campos son requiridos</div>';
+	}else{
+		$mail->From = $email;
+		$mail->FromName = $nombre;
+		$mail->addAddress($to);
+		$mail->Subject = $asunto;
+		$mail->isHtml(true);
+		$mail->Body = '<strong>'.$nombre.'</strong>'.' Le ha contactado desde su Web y le ha enviado el siguiente mensaje: <br><p>'.$mensaje.'</p>';
+		$mail->CharSet = 'UTF-8';
+		$mail->send();
+
+		if(!$mail){
+			echo '<div class="alert alert-danger">El mensaje no pudo ser Enviado<br>';
+			echo 'Mailer Error: '.$mail->ErrorInfo.'</div>';
+			echo '<script>$(#fcontacto).reset();</script>';
+		}else{
+			echo '<div class="alert alert-success">Ha enviado el mensaje satisfactoriamente.</div>';
+		}
+	}
+?>
